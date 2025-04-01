e670031ac8267a49216c1ec67d3a76126bc0455c
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-03-31 21:26:09 -0300

    update: add midleware alerts
    alerts for request sucess and response error endPoint Unkdown

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(requestLogger)
+
 let persons = [
   { 
     "id": 1,
@@ -29,7 +46,9 @@ let info = [{
   "date": new Date(),
   "persons": persons.length
 }]
+
 app.get('/', (request, response) => {
+  
     response.send('<h1>Hello World!</h1>')
   })
   
