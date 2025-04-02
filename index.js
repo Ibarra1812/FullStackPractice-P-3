e94c70e8883c35fe98a393e9f8ffe8bf48fe1c6d
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-04-02 13:55:59 -0300

    fix: add cors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
+
+
 app.use(express.json())
 app.use(express.static('dist'))
 const requestLogger = (request, response, next) => {
