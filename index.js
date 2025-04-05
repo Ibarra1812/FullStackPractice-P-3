a557283f37b5ac2d68f32ef87c4058e64cad2f11
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-04-05 19:43:56 -0300

    Update

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+require('dotenv').config() // Load environment variables from .env file
+//this is imported before importing notes.js to make sure that the environment variables are loaded before the database connection is made
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors())
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+const mongoose = require('mongoose')
+const Note = require('./models/note') // Import the Note model from models/note.js
+
+// Connect to MongoDB using the MONGO_URI environment variable
+const mongoUrl = process.env.MONGO_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())// Enable CORS for all routes, allowing cross-origin requests 
+// This is important for frontend to access the backend API beacause they are on different ports
+
+app.use(express.static('dist')) // Serve static files from the dist directory
+// This is the directory where the minified files of frontend are located, whith this connect frontend to backend
+
+let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -28,60 +32,83 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(requestLogger)
-app.use(express.static('dist'))
-app.use(express.json())
+app.use(requestLogger) // Middleware to log requests
+app.use(express.json()) // Middleware to parse JSON bodies
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
+
+/*  console.log(error) la vieja fomra de hacer el error sin el middleware
+      rresponse.status(400).send({ error: 'malformatted id' }) */
+
+
+
+
+/* const generateId = () => {
   const maxId =
     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
   return String(maxId + 1)
-}
+} */
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote) // savedNote is the note that was saved in the database
+    //and it is returned as a JSON with the method toJSON that is defined in the note.js file
+  })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -90,7 +117,20 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
+
+const PORT = process.env.PORT //process.env.PORT is used for a port number in production, and 3001 is used for development
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
