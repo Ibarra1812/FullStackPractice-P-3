6d5fe6df211b85697d1b01b75cc755830ab10505
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-04-09 22:30:25 -0300

    realise 4-1

--- index.js
+++ index.js
@@ -1,152 +1,47 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-var morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./modules/persons')
-app.use(cors())
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-
-app.use(express.json())
-app.use(express.static('dist'))
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-let persons = []
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-
-app.use(requestLogger)
-
-app.get('/info', async (request, response) => {
-  try {
-    const count = await Person.estimatedDocumentCount()
-    //Model.estimatedDocumentCount() is a method that provides an estimate of the number of documents in a collection without actually counting them.
-    response.send(`<div>Phonebook has info for ${count} people<div/>
-    <br/> 
-    <div>${new Date()}</div>`)
-  } catch (error) {
-    response.status(500).send({ error: 'Failed to fetch data' })
-  }
-})
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
-})
-
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-    .catch(error => next(error))
-})
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body
-
-  Person.findByIdAndUpdate(request.params.id,
-    { name, number },
-    { new: true, runValidators: true, context: 'query' })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
-})
-
-/* const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  } */
-/*  const getRandomInt = (max) => {
-      return Math.floor(Math.random() * max);
-    } */
-
-app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    if (body.name) {
-      return response.status(400).json({
-        error: 'number missing'
-      })
-    }
-    if (body.number) {
-      return response.status(400).json({
-        error: 'name missing'
-      })
-    }
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-  if( persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  }).catch(error => {
-    next(error)
-  })
-})
-
-
-
-/*   EXAMPLE: only log error responses
-morgan('combined', {
-  skip: function (req, res) { return res.statusCode < 400 }
-}) */
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
+const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
+
+
+console.log('connecting to', MONGODB_URI)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
