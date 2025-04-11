3e421bb3d27ff4901d271071f84efc513cb6bd38
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-04-11 00:07:43 -0300

    Realise Part-4-5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,22 @@ const totalLikes = (blogs) => {
   if (blogs.length === 1) return blogs[0].likes
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  const maxLikes = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+    })
+  const objectMaxLikes = {
+    title: maxLikes.title,
+    author: maxLikes.author,
+    likes: maxLikes.likes
+  }
+  return objectMaxLikes
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
